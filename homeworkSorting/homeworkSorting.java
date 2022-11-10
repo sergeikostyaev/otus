b941871897cdfa13912d27c467492c552a89c68f
sergeikostyaev <sergei.kostyaev88@gmail.com>
2022-11-10 17:25:28 +0300

    hw5 commit

--- homeworkSorting/homeworkSorting.java
+++ homeworkSorting/homeworkSorting.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class homeworkSorting {
     public static void main(String[] args) {
 
-        int[] array = new int[1000000];
+        int[] array = new int[10000];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random()*1000);
         }
@@ -14,12 +14,12 @@ public class homeworkSorting {
         long start = System.nanoTime();
         Arrays.sort(array);
         long finish = System.nanoTime();
-        System.out.println("Системная сортировка. Прошло мс: " + (finish-start));
+        System.out.println("Системная сортировка. Прошло нано: " + (finish-start));
 
         start = System.nanoTime();
         QuickSort.sort(array2, 0, array.length - 1);
         finish = System.nanoTime();
-        System.out.println("Моя сортировка. Прошло мс: " + (finish-start));
+        System.out.println("Моя сортировка. Прошло нано: " + (finish-start));
 
     }
 }
