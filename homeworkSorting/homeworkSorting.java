6cf2fab1710f6c22c87eafd5f5ae69811b093f4f
sergeikostyaev <sergei.kostyaev88@gmail.com>
2022-11-12 16:08:21 +0300

    hw6 commit

--- homeworkSorting/homeworkSorting.java
+++ homeworkSorting/homeworkSorting.java
@@ -5,21 +5,29 @@ import java.util.Arrays;
 public class homeworkSorting {
     public static void main(String[] args) {
 
-        int[] array = new int[10000];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random()*1000);
-        }
-        int[] array2 = array.clone();
+
 
         long start = System.nanoTime();
-        Arrays.sort(array);
+        for (int i = 0; i < 100; i++) {
+            int[] array = new int[1000000];
+            for (int j = 0; j < array.length; j++) {
+                array[j] = (int) (Math.random()*1000);
+            }
+            Arrays.sort(array);
+        }
         long finish = System.nanoTime();
-        System.out.println("Системная сортировка. Прошло нано: " + (finish-start));
+        System.out.println("Системная сортировка на 100 прогонов. Прошло нано: " + (finish-start));
 
         start = System.nanoTime();
-        QuickSort.sort(array2, 0, array.length - 1);
+        for (int i = 0; i < 100; i++) {
+            int[] array = new int[1000000];
+            for (int j = 0; j < array.length; j++) {
+                array[j] = (int) (Math.random()*1000);
+            }
+            QuickSort.sort(array, 0, array.length - 1);
+        }
         finish = System.nanoTime();
-        System.out.println("Моя сортировка. Прошло нано: " + (finish-start));
+        System.out.println("Моя сортировка на 100 прогонов. Прошло нано: " + (finish-start));
 
     }
 }
