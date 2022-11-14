0c27c4a00397db1d3db123c8bf38c5adf611376c
sergeikostyaev <sergei.kostyaev88@gmail.com>
2022-11-14 16:40:36 +0300

    hw6 commit

--- homeworkCollections/homeworkCollections.java
+++ homeworkCollections/homeworkCollections.java
@@ -1,44 +1,40 @@
 package otus.homeworkCollections;
 
-
 import java.util.*;
 
 public class homeworkCollections {
     public static void main(String[] args) {
-
         Bank b = new Bank();
-        b.addClient("Петир Паршенко", 54);
-        b.addClient("Денис Петров", 21);
-        List<Bank.Client> al = b.getClientList();
-        Bank.Client Petir = al.get(0);
-        Bank.Client Denis = al.get(1);
-        Petir.createAccount();
-        Petir.createAccount();
-        Petir.createAccount();
-        Denis.createAccount();
-        Denis.createAccount();
-
-        List<Bank.Account> petirList = b.getAllClientsAccounts(Petir);
-        for (Bank.Account a : petirList) {
-            System.out.println("Счет Петира Паршенко: " + a.getAccountNumber());
+        b.createClient("Петр Алексеев", 45);
+        b.createClient("Денис Петров", 24);
+        List<Client> l = b.getClientsList();
+        b.createClientAccount(l.get(0));
+        b.createClientAccount(l.get(0));
+        b.createClientAccount(l.get(1));
+        System.out.println("Список клиентов и счетов:");
+        for (Client c : l) {
+            for (Account a : c.getClientAccountList()) {
+                System.out.println(a.getAccountNumber() + " принадлежит клиенту - " + c.getClientName());
+            }
         }
 
-        System.out.println("");
-
-        List<Bank.Account> denisList = b.getAllClientsAccounts(Denis);
-        for (Bank.Account a : denisList) {
-            System.out.println("Счет Дениса Петрова: " + a.getAccountNumber());
-        }
-        System.out.println();
-
-        try (Scanner sc = new Scanner(System.in)){
-            System.out.println(b.findClient(sc.nextLong()));
+        System.out.println("\nПоиск клиента по номеру счета. Введите номер:");
+        try (Scanner sc = new Scanner(System.in)) {
+            long acc = sc.nextLong();
+            System.out.printf("Счет %d принадлежит клиенту %s\n", acc, b.findClientByAccount(acc));
         }
 
+        System.out.println("\nПоиск всех счетов по клиенту Петр Алексеев через equals и hashCode:");
+        Client testClient = new Client("Петр Алексеев", 45);
 
+        for (Client c : l) {
+            if (c.equals(testClient) && c.hashCode() == testClient.hashCode()) {
+                for (Account a : c.getClientAccountList()) {
+                    System.out.println(a.getAccountNumber());
+                }
+            }
+        }
     }
-
 }
-
 
 
