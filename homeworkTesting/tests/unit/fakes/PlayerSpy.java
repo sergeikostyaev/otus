36a3c86b036b59e656b0f89d9f8d386fbcd0704e
sergeikostyaev <116896578+sergeikostyaev@users.noreply.github.com>
2022-12-17 13:09:16 +0300

    Delete PlayerSpy.java

--- homeworkTesting/tests/unit/fakes/PlayerSpy.java
+++
@@ -1,9 +0,0 @@
-package otus.homeworkTesting.tests.unit.fakes;
-
-import otus.homeworkTesting.game.Player;
-
-public class PlayerSpy extends Player {
-    public PlayerSpy(String name) {
-        super(name);
-    }
-}
